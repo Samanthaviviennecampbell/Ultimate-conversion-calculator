fed5e3c5614f2d48af44da3abc5d0f6b464dbcb9
Samanthaviviennecampbell <Campbells4276@masseyhigh.school.nz>
2025-11-21 13:27:41 +1300

    Add files via upload

--- B_01_Conversion_Calculator.py
+++ B_01_Conversion_Calculator.py
@@ -8,10 +8,12 @@ def instructions():
         statement_generator("Instructions", "-")
 
         print('''
-       Instructions go here.
-        - instruction 1
-        _ instruction 2
-        - etc
+      Instructions
+      1) give a calc_type (time,mass,distance)
+      2) give the amount / quantity  of the calc_type
+      3) gve the unit you would like to change from
+      4) give the unit you would like to change this to
+      5) if you want to keep using the program enter another calc_type but if you want to finish code type xxx
         ''')
 
 
@@ -37,6 +39,19 @@ def num_check(question):
 
 
 # Main Routine Goes Here
+statement_generator("The ultimate Conversion Calculator", "-")
+
+# Display instructions if requested
+want_instructions = input("\nPress <enter> to read the instructions"
+                          "or any key to continue")
+
+if want_instructions == "":
+    instructions()
+
+# define domains
+valid_type = ["time", "mass" , "distance"]
+
+# Set up dictionaries
 time_dict = {
     "ms":3600000 ,
     "s": 3600,
@@ -46,29 +61,6 @@ time_dict = {
     "m":1/168,
     "y": 1 / (24 * 365 + 6 + 9/60)  # it accounts for leap years
 }
-
-# Get amount and units (assume they are valid)
-amount = float(input("how much?"))
-from_unit = input("from unit?")
-to_unit = input("To Unit?")
-
-# Multiply to get our standard value...
-multiply_by = time_dict[to_unit]
-print(multiply_by)
-standard = amount * multiply_by
-print(f"there are {standard} m in {from_unit}")
-
-# Divide to get to our desired value
-divide_by = time_dict[from_unit]
-answer = standard / divide_by
-
-# Look up value
-# multiply_by = distance_dict[to_unit]
-# answer = amount * multiply_by
-
-print(f"There are {answer} {to_unit} in {amount} {from_unit}")
-
-
 
 mass_dict = {
     "mg": 1000,
@@ -77,56 +69,50 @@ mass_dict = {
     "t": .000001,
 }
 
-# Get amount and units (assume they are valid)
-amount = float(input("how much?"))
-from_unit = input("from unit?")
-to_unit = input("To Unit?")
-
-# Multiply to get our standard value...
-multiply_by = mass_dict[to_unit]
-print(multiply_by)
-standard = amount * multiply_by
-print(f"there are {standard} m in {from_unit}")
-
-# Divide to get to our desired value
-divide_by = mass_dict[from_unit]
-answer = standard / divide_by
-
-# Look up value
-# multiply_by = distance_dict[to_unit]
-# answer = amount * multiply_by
-
-print(f"There are {answer} {to_unit} in {amount} {from_unit}")
-
-
-
-time_dict = {
-    "ms":3600000 ,
-    "s": 3600,
-    "min":60,
-    "h": 1,
-    "d": 1/24,
-    "m":1/168,
-    "y": 1 / (24 * 365 + 6 + 9/60)  # it accounts for leap years
+distance_dict = {
+    "mm" : 1000,
+    "cm" :100,
+    "m" : 1 ,
+    "km" :0.001
 }
 
-# Get amount and units (assume they are valid)
-amount = float(input("how much?"))
-from_unit = input("from unit?")
-to_unit = input("To Unit?")
+while True:
+    calc_type = input("calc_type: ").strip().lower()
+    if calc_type == "xxx":
+        print("Thank you using conversion calculator!!!^-^")
+        break
+    if calc_type not in valid_type:
+        print("Please enter a valid calculation type.")
+        continue
 
-# Multiply to get our standard value...
-multiply_by = time_dict[to_unit]
-print(multiply_by)
-standard = amount * multiply_by
-print(f"there are {standard} m in {from_unit}")
+    # Get amount and units (assume they are valid)
+    amount = float(input("how much?"))
+    from_unit = input("from unit?")
+    to_unit = input("To Unit?")
 
-# Divide to get to our desired value
-divide_by = time_dict[from_unit]
-answer = standard / divide_by
+    #set up loop
+    if calc_type == "time":
+        dictionary = time_dict
 
-# Look up value
-# multiply_by = distance_dict[to_unit]
-# answer = amount * multiply_by
+    elif calc_type == "mass":
+        dictionary = mass_dict
 
-print(f"There are {answer} {to_unit} in {amount} {from_unit}")
+    else:
+        dictionary = distance_dict
+
+    # Multiply to get our standard value...
+    multiply_by = dictionary[to_unit]
+    print(multiply_by)
+    standard = amount * multiply_by
+
+    # Divide to get to our desired value
+    divide_by = dictionary[from_unit]
+    answer = standard / divide_by
+
+    # Look up value
+    # multiply_by = distance_dict[to_unit]
+    # answer = amount * multiply_by
+
+    print(f"There are {answer} {to_unit} in {amount} {from_unit}")
+
+
